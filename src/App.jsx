52cd24f757e06ff503bf53781478a0ca17a2166e
amndalsr <laisamanda2015@gmail.com>
2024-07-17 17:55:14 -0300

    feat: add MenuLink component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
 import Banner from "./components/Banner";
+import Menu from "./components/Menu";
 
 function App() {
-  return <Banner />;
+  return (
+    <BrowserRouter>
+      <Menu />;
+      <Banner />
+    </BrowserRouter>
+  );
 }
 
 export default App;
