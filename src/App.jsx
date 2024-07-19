52a34c6ea51896489081eaaf78ddcbf6610cdaf1
amndalsr <laisamanda2015@gmail.com>
2024-07-19 15:45:33 -0300

    feat: update main App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
-import { BrowserRouter } from "react-router-dom";
-import Banner from "./components/Banner";
-import Menu from "./components/Menu";
+import { useRef } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AppointmentPage from "./pages/AppointmentPage";
+import AppointmentsListPage from "./pages/AppointmentsListPage";
+import Navbar from "./components/Navbar/Navbar";
+import Modal from "./components/Modal/Modal";
+import { ModalProvider } from "./context/ModalContext";
 
-function App() {
+const App = () => {
+  const appointmentsListRef = useRef();
+
+  const refreshAppointments = () => {
+    if (appointmentsListRef.current) {
+      appointmentsListRef.current.fetchAppointments();
+    }
+  };
+
   return (
-    <BrowserRouter>
-      <Menu />
-      <Banner />
-    </BrowserRouter>
+    <ModalProvider>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <AppointmentPage refreshAppointments={refreshAppointments} />
+              }
+            />
+            <Route path="/appointments" element={<AppointmentsListPage />} />
+          </Routes>
+          <Modal />
+        </div>
+      </Router>
+    </ModalProvider>
   );
-}
+};
+
+App.displayName = "App";
 
 export default App;
