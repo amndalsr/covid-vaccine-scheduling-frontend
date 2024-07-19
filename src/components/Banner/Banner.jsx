8d0037a561f57181aa1475a37e1daabdc5f5e662
amndalsr <laisamanda2015@gmail.com>
2024-07-19 15:39:14 -0300

    feat: add Banner component

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <h1>Sistema de Agendamento de Vacinação COVID-19</h1>
+      <p>Por favor, preencha o formulário abaixo para agendar sua vacinação.</p>
+    </div>
+  );
+};
+
+export default Banner;
